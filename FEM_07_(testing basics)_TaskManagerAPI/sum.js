a7cc3829f962e97b0a08be4a768c4666f887e270
NZIBUKA Emmanuel <emmynzibk21@gmail.com>
2026-08-09 17:43:00 +0200

    test: add async promise fetch example with resolves and async/await tests

--- FEM_07_(testing basics)_TaskManagerAPI/sum.js
+++ FEM_07_(testing basics)_TaskManagerAPI/sum.js
@@ -1,5 +1,16 @@
-const sum=(a, b) => {
-    return a + b;
-};
+// const sum=(a, b) => {
+//     return a + b;
+// };
 
-module.exports = sum;
+// module.exports = sum;
+
+function fetchPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Peanut butter');
+        }, 1000);
+    });
+}
+
+module.exports = fetchPromise;
+
--- FEM_07_(testing basics)_TaskManagerAPI/sum.test.js
+++ FEM_07_(testing basics)_TaskManagerAPI/sum.test.js
@@ -1,4 +1,15 @@
-const sum= require('./sum')
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+// const sum= require('./sum')
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3);
+// });
+
+const fetchPromise = require('./sum');
+
+test('the data is peanut butter', () => {
+    return expect(fetchPromise()).resolves.toBe('Peanut butter');
+});
+
+test('the data is peanut butter', async () => {
+    const data = await fetchPromise();
+    expect(data).toBe('Peanut butter');
 });
